88f37cbe119caa773215a268436f506c80d8f67f
AlexTemirov <temiroff@gmail.com>
2026-08-04 16:50:12 -0700

    release: bump runtime to 0.4.3

--- blacknode_runtime/__init__.py
+++ blacknode_runtime/__init__.py
@@ -1,6 +1,6 @@
 """Blacknode remote deployment runtime."""
 
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 
 from .config import RuntimeConfig
 from .deployments import DeploymentError, DeploymentStore
