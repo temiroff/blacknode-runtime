b85f5d67741206a8692a1598d85b426d17c59b64
AlexTemirov <temiroff@gmail.com>
2026-08-05 11:07:20 -0700

    Add remote ROS2 image transport

--- blacknode_runtime/__init__.py
+++ blacknode_runtime/__init__.py
@@ -1,16 +1,19 @@
 """Blacknode remote deployment runtime."""
 
-__version__ = "0.4.9"
+__version__ = "0.4.10"
 
 from .config import RuntimeConfig
 from .deployments import DeploymentError, DeploymentStore
 from .manifest import runtime_manifest
+from .ros2_images import Ros2ImageStreamError, Ros2ImageStreamStore
 from .ros2_streams import Ros2TopicStreamError, Ros2TopicStreamStore
 
 __all__ = [
     "DeploymentError",
     "DeploymentStore",
     "RuntimeConfig",
+    "Ros2ImageStreamError",
+    "Ros2ImageStreamStore",
     "Ros2TopicStreamError",
     "Ros2TopicStreamStore",
     "runtime_manifest",
