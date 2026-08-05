fba1dc307b8f022078d61d73b828bbbf521b7754
AlexTemirov <temiroff@gmail.com>
2026-08-04 22:18:21 -0700

    Release blacknode-runtime 0.4.8

--- blacknode_runtime/__init__.py
+++ blacknode_runtime/__init__.py
@@ -1,6 +1,6 @@
 """Blacknode remote deployment runtime."""
 
-__version__ = "0.4.7"
+__version__ = "0.4.8"
 
 from .config import RuntimeConfig
 from .deployments import DeploymentError, DeploymentStore
