f72c8327110bea2f4290520b9bc9339ffc961193
AlexTemirov <temiroff@gmail.com>
2026-08-07 16:11:17 -0700

    Preserve runtime service configuration on update

--- blacknode_runtime/__init__.py
+++ blacknode_runtime/__init__.py
@@ -1,6 +1,6 @@
 """Blacknode remote deployment runtime."""
 
-__version__ = "0.4.11"
+__version__ = "0.4.12"
 
 from .config import RuntimeConfig
 from .deployments import DeploymentError, DeploymentStore
